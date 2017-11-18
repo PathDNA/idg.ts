843e7b41663adc1b431c7e07d79f6949f6d6ebcf
itsMontoya <josh@montoya.io>
2017-11-18 10:54:11 -0800

    Add better commenting

--- idg.js
+++ idg.js
@@ -10,33 +10,48 @@ define("idg", ["require", "exports"], function (require, exports) {
             return this.index;
         }
         Date() {
+            // Timestamp is stored as seconds, Javascript Date object is parsed as milliseconds
             return new Date(this.ts * 1000);
         }
     }
     exports.ID = ID;
     function Parse(str) {
+        // Decode base64 string
         const decoded = atob(str);
+        // Check if the decoded length is proper
         if (decoded.length !== 16) {
             throw ("invalid string length, expected 16 and received " + decoded.length);
         }
+        // Get index bytes from first 8 characters
         const indexBytes = getBytes(decoded.substring(0, 8));
+        // Get timestamp bytes from last 8 characters
         const tsBytes = getBytes(decoded.substring(8, 16));
         return new ID(parseBytes(indexBytes), parseBytes(tsBytes));
     }
     exports.Parse = Parse;
     function getBytes(str) {
+        // Initialize array
         const bytes = new Uint8Array(8);
+        // Array index
         let j = 8;
+        // String index
+        let i = 0;
+        // Iterate through all indexes
         while (j--) {
-            bytes[j] = str.charCodeAt(7 - j);
+            // Since our string is little endian, we need to populate the array in reverse
+            bytes[j] = str.charCodeAt(i++);
         }
         return bytes;
     }
     function parseBytes(bytes) {
+        // Return value
         var n = 0;
+        // Iterate through each byte
         bytes.forEach(function (byte, i) {
+            // Increment value by byte-value
             n += byte;
             if (i < bytes.length - 1) {
+                // We are not at the last byte, perform a bit shift
                 n = n << 8;
             }
         });
--- idg.ts
+++ idg.ts
@@ -1,5 +1,8 @@
 export class ID {
+	// Index (uint64, beware of overflow)
+	// TODO: Implement uint64 support so this is proper
 	private index: number;
+	// Timestamp (in seconds)
 	private ts: number;
 
 	constructor(index: number, ts: number) {
@@ -12,27 +15,36 @@ export class ID {
 	}
 
 	Date(): Date {
+		// Timestamp is stored as seconds, Javascript Date object is parsed as milliseconds
 		return new Date(this.ts * 1000);
 	}
 }
 
 export function Parse(str: string): ID {
+	// Decode base64 string
 	const decoded = atob(str);
+	// Check if the decoded length is proper
 	if (decoded.length !== 16) {
 		throw ("invalid string length, expected 16 and received " + decoded.length);
 	}
-
+	// Get index bytes from first 8 characters
 	const indexBytes = getBytes(decoded.substring(0, 8));
+	// Get timestamp bytes from last 8 characters
 	const tsBytes = getBytes(decoded.substring(8, 16));
 	return new ID(parseBytes(indexBytes), parseBytes(tsBytes))
 }
 
 function getBytes(str: string): Uint8Array {
+	// Initialize array
 	const bytes = new Uint8Array(8);
+	// Array index
 	let j = 8;
-
+	// String index
+	let i = 0;
+	// Iterate through all indexes
 	while (j--) {
-		bytes[j] = str.charCodeAt(7 - j);
+		// Since our string is little endian, we need to populate the array in reverse
+		bytes[j] = str.charCodeAt(i++);
 	}
 
 	return bytes;
@@ -40,10 +52,14 @@ function getBytes(str: string): Uint8Array {
 
 
 function parseBytes(bytes: Uint8Array): number {
+	// Return value
 	var n = 0;
+	// Iterate through each byte
 	bytes.forEach(function (byte: number, i: number) {
+		// Increment value by byte-value
 		n += byte;
 		if (i < bytes.length - 1) {
+			// We are not at the last byte, perform a bit shift
 			n = n << 8;
 		}
 	});
